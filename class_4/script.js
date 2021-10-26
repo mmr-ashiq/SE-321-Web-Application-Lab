bf0675f2838134da75b9591627996a113d15d5bb
Md. Mizanur Rahman Ashiq <mizanur35-2844@diu.edu.bd>
2021-10-26 07:56:00 -0700

    Update script.js

--- class_4/script.js
+++ class_4/script.js
@@ -1,7 +1,7 @@
 fetch("https://api-football-v1.p.rapidapi.com/v3/teams?id=33", {
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+		"x-rapidapi-host": "your_api_key",
 		"x-rapidapi-key": "4dfb016a0bmsh7dc911e2ecf70cep1ade67jsn9ff7c0ea8e35"
 	}
 })
